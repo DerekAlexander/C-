f6639a713b8c9d0cdb0e344ef9c32bb35b93ab61
DerekAlexander <42855738+DerekAlexander@users.noreply.github.com>
2018-09-12 18:44:20 -0500

    Create Resource.h

---
+++ Rectangles/Resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by AlexanderDerekRectangle.rc
+//
+#define IDC_MYICON                      2
+#define IDD_ALEXANDERDEREKRECTANGLE_DIALOG 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_ALEXANDERDEREKRECTANGLE     107
+#define IDI_SMALL                       108
+#define IDC_ALEXANDERDEREKRECTANGLE     109
+#define IDR_MAINFRAME                   128
+#define ID_OPTIONS_RECTANGLE            32771
+#define ID_OPTIONS_ELLIPSE              32772
+#define ID_OPTIONS_LINES                32773
+#define ID_OPTIONS_TIMER                32774
+#define ID_OPTIONS_EXTRACREDIT          32775
+#define ID_OPTIONS_CONCENTRIC           32776
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
